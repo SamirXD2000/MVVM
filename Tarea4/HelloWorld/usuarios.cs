993d808e0d0c3b19cc4c78fc1d6a5f7edfe65c16
SamirXD2000 <47357184+SamirXD2000@users.noreply.github.com>
2019-02-18 09:22:52 -0600

    tareas 4 5

---
+++ Tarea4/HelloWorld/usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class usuarios
+    {
+        string usuario;
+        string passwd;
+
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Passwd { get => passwd; set => passwd = value; }
+
+    }
+}
